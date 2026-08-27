7689453a033b6be422bd97689a80ca7dd88c2f52
Aripirala Chaitanya <aripiralachaitanya.new@gmail.com>
2026-08-27 13:40:35 +0530

    feat: update API base URL to Render backend deployment

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://lostlink-qul8.onrender.com/api',
 });
 
 // Interceptor to automatically add Authorization token to headers
